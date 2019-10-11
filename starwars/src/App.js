5409262e41fe58f4e738437fc43c7d43512b62d4
occludedarp <cmrnmr@gmail.com>
2019-10-11 16:39:33 -0400

    added outlines for useEffect

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+const [starWarsChar, setStarWarsChar] = useState([]);
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+useEffect(() => {
+  axios.get(`https://swapi.co/api/people/`)
+    .then( response => {
+      console.log(response.data.results)
+      // setStarWarsChar(response.data.results)
+    })
+    .catch( error => {
+      console.log( "no data was returned", error)
+    })
+}, [])
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      {/* <p>{starWarsChar.map((details) => (
+        characterName={details.name}
+      ))}
+      </p> */}
     </div>
   );
 }
